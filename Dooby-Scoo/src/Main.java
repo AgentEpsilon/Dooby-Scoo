16a5d6a242c375e36da5c7fd876e84cc40a6a649
flaneflantastico <miller.evan815@gmail.com>
2013-11-22 11:09:18 -0500

    Added Test introduction.

--- Dooby-Scoo/src/Main.java
+++ Dooby-Scoo/src/Main.java
@@ -1,4 +1,10 @@
 
 public class Main {
 
+	public static void main(String[] args)
+	{
+		System.out.println("Dooby-Scoo walked into the mansion.");
+		System.out.println("What should Dooby-Scoo do?");
+	}
+	
 }
