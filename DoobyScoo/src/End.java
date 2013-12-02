1f13a24506782177b9dc969b9a45c3cb53cbaa96
flaneflantastico <miller.evan815@gmail.com>
2013-12-02 15:17:11 -0500

    Added Retry catch

--- DoobyScoo/src/End.java
+++ DoobyScoo/src/End.java
@@ -12,8 +12,17 @@ public class End {
 		String endMessage = "Congratulations! You solved the mystery!\nThanks for playing!";
 		if(death)
 		{
-			endMessage = "Oh No! Looks like you met a bad end.\nTry again for a better ending!";
-			JOptionPane.showMessageDialog(null, endMessage, "The End", JOptionPane.ERROR_MESSAGE);
+			endMessage = "Oh No! Looks like you met a bad end.\nWould you like to try again?";
+			Object[] choices = {"Retry", "Quit"};
+			int choice = JOptionPane.showOptionDialog(null, endMessage, "The End", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, null, choices, null);
+			switch(choice)
+			{
+			case 0:
+				Main.main(null);
+				break;
+			case 1:
+				break;
+			}
 		}else{
 		JOptionPane.showMessageDialog(null, endMessage, "The End", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(getClass().getResource("meddlingkids1.jpg")));
 		}
